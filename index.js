f19195744999f1171df69e3af26f5d220bdd258a
Putu Gde Arya Bhanuartha <tuaryabhanu03@gmail.com>
2023-06-06 17:55:51 +0700

    cleaning

--- index.js
+++ index.js
@@ -40,10 +40,7 @@ app.use(function (err, req, res, next) {
 	});
 });
 
-// app.use(function (req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-// 	next();
-// });
+
 
 // routes
 app.use("/admin", adminRoutes);
